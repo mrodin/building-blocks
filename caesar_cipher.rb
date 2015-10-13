36c3a9227e474034ce484d8cc525ae62bc6bc595
Martin Rodin <rodin.martin@gmail.com>
2015-10-13 16:10:03 +0200

    small tweaks

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,35 +1,28 @@
 # Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
-#
 # > caesar_cipher("What a string!", 5)
 # => "Bmfy f xywnsl!"
 
 def caesar_cipher (input, shift)
-  lowcase_alph = ('a'..'z')
-  upcase_alph = ('A'..'Z')
+  alphabet = [*'a'..'z', *'A'..'Z']
+  split_arr = input.split('')
+  result = []
 
-  split_ary = input.split('')
-  ascii_ary = []
-
-  split_ary.each do |c|
-    if (lowcase_alph.include?(c))
-      if (c.ord + shift > 122)
-        ascii_ary << (c.ord - 26) + shift
+  split_arr.each do |c|
+    if (alphabet.include?(c))
+      i = alphabet.index(c) + (shift % 26)
+      if (i > alphabet.length && /[[:upper:]]/.match(c))
+        result << alphabet[i - 26]
+      elsif (i > alphabet.length)
+        result << alphabet[i - 52]
       else
-        ascii_ary << c.ord + shift
-      end
-    elsif (upcase_alph.include?(c))
-      if (c.ord + shift > 90)
-        ascii_ary << (c.ord - 26) + shift
-      else
-        ascii_ary << c.ord + shift
+        result << alphabet[i]
       end
     else
-      ascii_ary << c.ord
+      result << c
     end
   end
 
-  output = ascii_ary.map { |x| x.chr }
-  puts output.join('')
+  return result.join('')
 end
 
-caesar_cipher('What a string!', 5)
+puts caesar_cipher('What a string!', 5)
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,20 @@
+# Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day.
+# It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
 def stock_picker (stocks)
+  result = []
   diff = 0
-  buy_position = 0
-  sell_position = 0
 
   stocks[0..-2].each do |stock|
     stocks[stocks.index(stock)..-1].each do |s|
       if (stock - s < diff)
         diff = stock - s
-        buy_position = stocks.index(stock)
-        sell_position = stocks.index(s)
+        result[0] = stocks.index(stock)
+        result[1] = stocks.index(s)
       end
     end
   end
-
-  puts "[#{buy_position},#{sell_position}]"
+  return result
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+puts stock_picker([17,3,6,9,15,8,6,1,10])
